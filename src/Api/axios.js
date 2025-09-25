630a5259500e2c3fb970dad1cc923358be380cd7
kibran meng <kibran29mengistu@gmail.com>
2025-09-25 13:13:58 +0300

    Updated API baseURL to point to live backend

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: "http://127.0.0.1:5001/clone-8a6dc/us-central1/api",
+//deployed version of amazon server on render.com
+  baseURL: "https://amazon-api-deploy-1-avqw.onrender.com/",
 });
 
 export default axiosInstance;
